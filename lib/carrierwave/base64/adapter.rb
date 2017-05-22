1ec3e7c35ce939997faa8f2f4d269f91ddedd874
Martiniano Felici <martiniano.felici@gmail.com>
2017-05-22 19:01:21 -0300

    Implemented mount_base64_uploaders. It makes use of carrierwave's mount_uploaders method to upload multiple files.

--- lib/carrierwave/base64/adapter.rb
+++ lib/carrierwave/base64/adapter.rb
@@ -30,6 +30,37 @@ module Carrierwave
         # rubocop:enable Metrics/CyclomaticComplexity
         # rubocop:enable Metrics/PerceivedComplexity
       end
+
+      def mount_base64_uploaders(attribute, uploader_class, options = {})
+        mount_uploaders attribute, uploader_class, options
+        options[:file_name] ||= -> { attribute }
+
+        define_method "#{attribute}=" do |data|
+          return if data == send(attribute).to_s
+
+          if respond_to?("#{attribute}_will_change!") && data.present?
+            send "#{attribute}_will_change!"
+          end
+
+          data = [data].flatten
+
+          processed_data = data.map.with_index do |item, index|
+            if item.is_a?(String) && item.strip.start_with?('data')
+              filename = if options[:file_name].respond_to?(:call)
+                options[:file_name].call
+              else
+                options[:file_name].to_s
+              end
+
+              Carrierwave::Base64::Base64StringIO.new(item.strip, "#{filename}_#{index}")
+            else
+              item
+            end
+          end
+
+          super processed_data
+        end
+      end
     end
   end
 end
